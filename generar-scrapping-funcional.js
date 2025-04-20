83d88e8e1b84a707f6bafb76acfaf5081a38df2f
obfuentesp <obfuentesp@gmail.com>
2025-04-20 23:31:51 +0000

    se agregan test

--- generar-scrapping-funcional.js
+++ generar-scrapping-funcional.js
@@ -8,7 +8,7 @@ async function ejecutarScraping(url) {
     return null;
   }
 
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   try {
